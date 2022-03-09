d6f48ba9687203236e67d50204aeeb3d480a8f15
Iphycodes <ifeanyiemmanuel585@gmail.com>
2022-03-09 02:06:38 -0800

    just added the react-router-dom to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+// import {
+//   //Switch,
+//   Route
+//   //Link
+// } from 'react-router-dom'
+
+
+export const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+)
 
 
 const App = () => (
   <div className='app'>
-    <h1>react app</h1>
-    <Homepage/>
+    {/* <Route exact path = '/'> */}
+      <Homepage/>
+    {/* </Route> */}
   </div>
 )
 
